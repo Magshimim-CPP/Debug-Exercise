2775c6dca10262441d7f8414ca88f51423082e16
Ohad Shaked <ohad.saked@gmail.com>
2022-12-09 15:04:47 +0200

    fixed shapes

--- DebugWorkshop/Shapes/Shapes.cpp
+++ DebugWorkshop/Shapes/Shapes.cpp
@@ -8,7 +8,7 @@ int main()
 	Triangle triangle(2, 10);
 	std::cout << "The area of the triangle is " << triangle.get_area() << std::endl;
 
-	Shape* pTriangle = new Triangle(2, 10);
+	Shape* pTriangle = new Triangle(2, 10); //get_area methods didn't have the same signature.
 	std::cout << "The area of the triangle is " << pTriangle->get_area() << std::endl;
 	delete pTriangle;
 

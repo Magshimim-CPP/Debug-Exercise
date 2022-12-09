38fb5aabfb52e4d14045d5ee6d24a38d3d86cc4d
Ohad Shaked <ohad.saked@gmail.com>
2022-12-09 15:46:55 +0200

    got the password bug for Password.cpp

--- DebugWorkshop/Password/Password.cpp
+++ DebugWorkshop/Password/Password.cpp
@@ -14,7 +14,8 @@ int main()
 	std::cout << "Enter your password to continue:" << std::endl;
 	Password pwd;
 	std::cin >> pwd.value;
-
+	
+	//when you enter "********", the strcmp returns 0, and !0 gives us true. the incorrect field is set to false, and Congratulations is printed.
 	if (!strcmp(pwd.value, "********"))
 		pwd.incorrect = false;
 
